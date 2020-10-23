aa678012d258ce8536a432997a351a0a028a10d1
Dawit Ayana <dawitz28@gmail.com>
2020-10-22 19:33:57 -0700

    lab 07 complete

--- js/app.js
+++ js/app.js
@@ -1,248 +1,96 @@
 'use strict';
-// console.log('hello world');
-// min number of customers per hour 
-// max number of customers per hour
-// add and remove locations from daily report, numbers of each location day of the week, special events ..
-// construction of a public facing page (home page) logo provided,
-// build an app that calculates daily sales for each location on sales.html page
-// create homepage index.html 
 
 // number of cookies to make based on bisiness hours 6am - 8pm
+var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+var parentElement = document.getElementById('solmon cookies-page');
+var SalmonCookiesTable = document.getElementById('salmon-table');
+console.log(SalmonCookiesTable);
 
-var storeHours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
-
-var seattleUl = document.getElementById('seattle');
-var tokyoUl = document.getElementById('tokyo');
-var dubaiUl = document.getElementById('dubai');
-var parisUl = document.getElementById('paris');
-var limaUl = document.getElementById('lima');
-
-var seattle = {
-    minCust: 23,
-    maxCust: 65, 
-    avgHourlyCookies: 6.3,
-    cookiesPerHourArray:[],
-    finalDailySales: 0,
-    
-    // number of cookies each location make min and max. use Math.random()
-    randomNumCust : function (){
+//Salmon Cookies Constructor
+function Store(name, minCust, maxCust, avgHourlyCookies){
+    this.name = name;
+    this.minCust = minCust;
+    this.maxCust = maxCust;
+    this.avgHourlyCookies = avgHourlyCookies;
+    this.cookiesPerHourArray = [];
+    this.finalDailySales = 0;
+    this.randomNumCust = function (){
         return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-    },
-    // the average number of cookies purchesed per customers
-    calcCookiesPerHourArray: function(){
-        for (var i = 0; i < storeHours.length; i++){
+    };
+    this.calcCookiesPerHourArray = function(){
+        for (var i = 0; i < hours.length; i++){
             var hourlyCookies = Math.ceil(this.randomNumCust() * this.avgHourlyCookies); 
             this.cookiesPerHourArray.push(hourlyCookies);
             this.finalDailySales += hourlyCookies; 
         }
-        console.log('calc the amount of cookies bought per hour:', hourlyCookies);
-    },
-    render: function(){
+    };
+
+    this.render = function(){
         this.calcCookiesPerHourArray();
-
-        for (var i = 0; i < storeHours.length; i++){
-            var li = document.createElement('li');
-
-            li.textContent = `${storeHours[i]}:${this.cookiesPerHourArray[i]} cookies`;
-
-            seattleUl.appendChild(li);
-
+        //create row
+        var tr = document.createElement('tr');
+        SalmonCookiesTable.appendChild(tr);
+        //give it content 
+        var td = document.createElement('td');
+        //give td content
+        td.textContent = this.name;
+        //append to row
+        tr.appendChild(td);
+        //create tds for cookies per hour
+        for (var i = 0; i < hours.length; i++){
+            td = document.createElement('td');
+            td.textContent = this.cookiesPerHourArray[i];
+            tr.appendChild(td);
         }
-        li = document.createElement('li');
-
-        li.textContent = ` Total: ${this.finalDailySales} cookies`;
-
-        seattleUl.appendChild(li);
+        //give tds content
+        td = document.createElement('td');
+        //append to the row
+        td.textContent = this.finalDailySales
+        tr.appendChild(td);
+        //append to dom 
+        SalmonCookiesTable.appendChild(tr);
     }
-};
-var tokyo = {
-    minCust: 3,
-    maxCust: 24, 
-    avgHourlyCookies: 1.2,
-    cookiesPerHourArray:[],
-    finalDailySales: 0,
-    
-    // number of cookies each location make min and max. use Math.random()
-    randomNumCust : function (){
-        return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-    },
-    // the average number of cookies purchesed per customers
-    calcCookiesPerHourArray: function(){
-        for (var i = 0; i < storeHours.length; i++){
-            var hourlyCookies = Math.ceil(this.randomNumCust() * this.avgHourlyCookies); 
-            this.cookiesPerHourArray.push(hourlyCookies);
-            this.finalDailySales += hourlyCookies; 
-        }
-        console.log('calc the amount of cookies bought per hour:', hourlyCookies);
-    },
-    render: function(){
-        this.calcCookiesPerHourArray();
+}
 
-        for (var i = 0; i < storeHours.length; i++){
-            var li = document.createElement('li');
-
-            li.textContent = `${storeHours[i]}:${this.cookiesPerHourArray[i]} cookies`;
-
-            tokyoUl.appendChild(li);
-
-        }
-        li = document.createElement('li');
-
-        li.textContent = ` Total: ${this.finalDailySales} cookies`;
-
-        tokyoUl.appendChild(li);
+function renderHeader(){
+    var tr = document.createElement('tr');
+    SalmonCookiesTable.appendChild(tr);
+    //give it content 
+    var td = document.createElement('td');
+    //give td content
+    td.textContent = '  ';
+    //append to row
+    tr.appendChild(td);
+    //create tds for cookies per hour
+    for (var i = 0; i < hours.length; i++){
+        td = document.createElement('td');
+        td.textContent = hours[i];
+        tr.appendChild(td);
     }
-};
-var dubai = {
-    minCust: 11,
-    maxCust: 38, 
-    avgHourlyCookies: 3.7,
-    cookiesPerHourArray:[],
-    finalDailySales: 0,
-    
-    // number of cookies each location make min and max. use Math.random()
-    randomNumCust : function (){
-        return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-    },
-    // the average number of cookies purchesed per customers
-    calcCookiesPerHourArray: function(){
-        for (var i = 0; i < storeHours.length; i++){
-            var hourlyCookies = Math.ceil(this.randomNumCust() * this.avgHourlyCookies); 
-            this.cookiesPerHourArray.push(hourlyCookies);
-            this.finalDailySales += hourlyCookies; 
-        }
-        console.log('calc the amount of cookies bought per hour:', hourlyCookies);
-    },
-    render: function(){
-        this.calcCookiesPerHourArray();
+    //give tds content
+    td = document.createElement('td');
+    //append to the row
+    td.textContent = 'Total';
+    tr.appendChild(td);
+    //append to dom 
+    SalmonCookiesTable.appendChild(tr);
+}
 
-        for (var i = 0; i < storeHours.length; i++){
-            var li = document.createElement('li');
+var tfoot = document.createElement('tfoot');
+var td =document.createElement(td);
+td.textContent = 'Total';
+tfoot.appendChild(td);
+SalmonCookiesTable.appendChild(tfoot);
 
-            li.textContent = `${storeHours[i]}:${this.cookiesPerHourArray[i]} cookies`;
+var seattle = new Store('Seattle',23, 65, 6.3);
+var tokyo = new Store('Tokyo',3, 24, 1.2);
+var dubai = new Store('Dubai', 11, 38, 3.7);
+var paris = new Store('Paris', 20, 38, 2.3);
+var lima = new Store('Lima', 2, 16, 4.6);
 
-            dubaiUl.appendChild(li);
-
-        }
-        li = document.createElement('li');
-
-        li.textContent = ` Total: ${this.finalDailySales} cookies`;
-
-        dubaiUl.appendChild(li);
-    }
-};
-var paris = {
-    minCust: 20,
-    maxCust: 38, 
-    avgHourlyCookies: 2.3,
-    cookiesPerHourArray:[],
-    finalDailySales: 0,
-    
-    // number of cookies each location make min and max. use Math.random()
-    randomNumCust : function (){
-        return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-    },
-    // the average number of cookies purchesed per customers
-    calcCookiesPerHourArray: function(){
-        for (var i = 0; i < storeHours.length; i++){
-            var hourlyCookies = Math.ceil(this.randomNumCust() * this.avgHourlyCookies); 
-            this.cookiesPerHourArray.push(hourlyCookies);
-            this.finalDailySales += hourlyCookies; 
-        }
-        console.log('calc the amount of cookies bought per hour:', hourlyCookies);
-    },
-    render: function(){
-        this.calcCookiesPerHourArray();
-
-        for (var i = 0; i < storeHours.length; i++){
-            var li = document.createElement('li');
-
-            li.textContent = `${storeHours[i]}:${this.cookiesPerHourArray[i]} cookies`;
-
-            parisUl.appendChild(li);
-
-        }
-        li = document.createElement('li');
-
-        li.textContent = ` Total: ${this.finalDailySales} cookies`;
-
-        parisUl.appendChild(li);
-    }
-};
-var lima = {
-    minCust: 2,
-    maxCust: 16, 
-    avgHourlyCookies: 4.6,
-    cookiesPerHourArray:[],
-    finalDailySales: 0,
-    
-    // number of cookies each location make min and max. use Math.random()
-    randomNumCust : function (){
-        return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-    },
-    // the average number of cookies purchesed per customers
-    calcCookiesPerHourArray: function(){
-        for (var i = 0; i < storeHours.length; i++){
-            var hourlyCookies = Math.ceil(this.randomNumCust() * this.avgHourlyCookies); 
-            this.cookiesPerHourArray.push(hourlyCookies);
-            this.finalDailySales += hourlyCookies; 
-        }
-        console.log('calc the amount of cookies bought per hour:', hourlyCookies);
-    },
-    render: function(){
-        this.calcCookiesPerHourArray();
-
-        for (var i = 0; i < storeHours.length; i++){
-            var li = document.createElement('li');
-
-            li.textContent = `${storeHours[i]}:${this.cookiesPerHourArray[i]} cookies`;
-
-            limaUl.appendChild(li);
-
-        }
-        li = document.createElement('li');
-
-        li.textContent = ` Total: ${this.finalDailySales} cookies`;
-
-        limaUl.appendChild(li);
-    }
-};
-
+renderHeader();
 seattle.render(); 
 tokyo.render(); 
 dubai.render(); 
 paris.render(); 
 lima.render(); 
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Function Store(name, minCust, maxCust, avgCookiesCust){
-//     this.name = name;
-//     this.minCust = minCust;
-//     this.maxCust = maxCust;
-//     this.avgCookiesHour = avgCookiesHour;
-//     this.custPerHourArray = [];
-//     this.finalDailySales = 0;
-// }
